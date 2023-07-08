f7fd12fb0c893c194d28b16db0f9b8d08c22acb4
Kaupenjoe <N.Kaupenjohann@gmail.com>
2023-07-08 14:57:26 +0200

    tooltips

--- src/main/java/net/kaupenjoe/tutorialmod/block/custom/SoundBlock.java
+++ src/main/java/net/kaupenjoe/tutorialmod/block/custom/SoundBlock.java
@@ -2,14 +2,21 @@ package net.kaupenjoe.tutorialmod.block.custom;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class SoundBlock extends Block {
     public SoundBlock(Settings settings) {
@@ -21,5 +28,11 @@ public class SoundBlock extends Block {
                               PlayerEntity player, Hand hand, BlockHitResult hit) {
         world.playSound(player, pos, SoundEvents.BLOCK_NOTE_BLOCK_XYLOPHONE.value(), SoundCategory.BLOCKS, 1f, 1f);
         return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable BlockView world, List<Text> tooltip, TooltipContext options) {
+        tooltip.add(Text.literal("Makes a sweet sound when right-clicked"));
+        super.appendTooltip(stack, world, tooltip, options);
     }
 }
--- src/main/java/net/kaupenjoe/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/kaupenjoe/tutorialmod/item/custom/MetalDetectorItem.java
@@ -3,12 +3,18 @@ package net.kaupenjoe.tutorialmod.item.custom;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class MetalDetectorItem extends Item {
     public MetalDetectorItem(Settings settings) {
@@ -51,5 +57,11 @@ public class MetalDetectorItem extends Item {
 
     private boolean isValuableBlock(BlockState state) {
         return state.isOf(Blocks.IRON_ORE) || state.isOf(Blocks.DIAMOND_ORE);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.translatable("tooltip.tutorialmod.metal_detector.tooltip"));
+        super.appendTooltip(stack, world, tooltip, context);
     }
 }
