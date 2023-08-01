6fc3f2cd79e61975951b485600aaf442132e7e6f
Kaupenjoe <N.Kaupenjohann@gmail.com>
2023-08-01 17:27:29 +0200

    custom tags

--- src/main/java/net/kaupenjoe/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/kaupenjoe/tutorialmod/item/custom/MetalDetectorItem.java
@@ -1,5 +1,6 @@
 package net.kaupenjoe.tutorialmod.item.custom;
 
+import net.kaupenjoe.tutorialmod.util.ModTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
@@ -56,7 +57,7 @@ public class MetalDetectorItem extends Item {
     }
 
     private boolean isValuableBlock(BlockState state) {
-        return state.isOf(Blocks.IRON_ORE) || state.isOf(Blocks.DIAMOND_ORE);
+        return state.isIn(ModTags.Blocks.METAL_DETECTOR_DETECTABLE_BLOCKS);
     }
 
     @Override
---
+++ src/main/java/net/kaupenjoe/tutorialmod/util/ModTags.java
@@ -0,0 +1,27 @@
+package net.kaupenjoe.tutorialmod.util;
+
+import net.kaupenjoe.tutorialmod.TutorialMod;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+
+public class ModTags {
+    public static class Blocks {
+        public static final TagKey<Block> METAL_DETECTOR_DETECTABLE_BLOCKS =
+                createTag("metal_detector_detectable_blocks");
+
+        private static TagKey<Block> createTag(String name) {
+            return TagKey.of(RegistryKeys.BLOCK, new Identifier(TutorialMod.MOD_ID, name));
+        }
+    }
+
+    public static class Items {
+
+
+        private static TagKey<Item> createTag(String name) {
+            return TagKey.of(RegistryKeys.ITEM, new Identifier(TutorialMod.MOD_ID, name));
+        }
+    }
+}
