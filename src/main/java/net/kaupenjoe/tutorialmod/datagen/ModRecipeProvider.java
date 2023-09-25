a735f547a471f3f16012097d2a34f3b66e80e8ee
Kaupenjoe <N.Kaupenjohann@gmail.com>
2023-09-25 16:08:12 +0200

    update 1.20.2

--- src/main/java/net/kaupenjoe/tutorialmod/datagen/ModRecipeProvider.java
+++ src/main/java/net/kaupenjoe/tutorialmod/datagen/ModRecipeProvider.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.kaupenjoe.tutorialmod.block.ModBlocks;
 import net.kaupenjoe.tutorialmod.item.ModItems;
+import net.minecraft.data.server.recipe.RecipeExporter;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.item.ItemConvertible;
@@ -23,7 +24,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
     }
 
     @Override
-    public void generate(Consumer<RecipeJsonProvider> exporter) {
+    public void generate(RecipeExporter exporter) {
         offerSmelting(exporter, RUBY_SMELTABLES, RecipeCategory.MISC, ModItems.RUBY,
                 0.7f, 200, "ruby");
         offerBlasting(exporter, RUBY_SMELTABLES, RecipeCategory.MISC, ModItems.RUBY,
--- src/main/java/net/kaupenjoe/tutorialmod/util/ModLootTableModifiers.java
+++ src/main/java/net/kaupenjoe/tutorialmod/util/ModLootTableModifiers.java
@@ -49,14 +49,14 @@ public class ModLootTableModifiers {
         });
 
         LootTableEvents.REPLACE.register((resourceManager, lootManager, id, original, source) -> {
-            if(SUSPICIOUS_SAND_ID.equals(id)) {
-                List<LootPoolEntry> entries = new ArrayList<>(Arrays.asList(original.pools[0].entries));
-                entries.add(ItemEntry.builder(ModItems.METAL_DETECTOR).build());
-                entries.add(ItemEntry.builder(ModItems.COAL_BRIQUETTE).build());
-
-                LootPool.Builder pool = LootPool.builder().with(entries);
-                return LootTable.builder().pool(pool).build();
-            }
+            // if(SUSPICIOUS_SAND_ID.equals(id)) {
+            //     List<LootPoolEntry> entries = new ArrayList<>(Arrays.asList(original.pools[0].entries));
+            //     entries.add(ItemEntry.builder(ModItems.METAL_DETECTOR).build());
+            //     entries.add(ItemEntry.builder(ModItems.COAL_BRIQUETTE).build());
+//
+            //     LootPool.Builder pool = LootPool.builder().with(entries);
+            //     return LootTable.builder().pool(pool).build();
+            // }
 
             return null;
         });
